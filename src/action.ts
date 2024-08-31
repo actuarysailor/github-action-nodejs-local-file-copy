910bd40235ccc0d765503fb1fca138e0aa0dec3b
David White <24359398+actuarysailor@users.noreply.github.com>
2024-08-31 15:27:46 -0400

    :feat: Initial draft of file copy action

--- src/action.ts
+++ src/action.ts
@@ -2,6 +2,9 @@ import { Inputs } from "./inputs/inputs";
 import { Logger } from "./logger/logger";
 import { Outputs } from "./outputs/outputs";
 import { sleep } from "./utils/sleep";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as glob from 'glob';
 
 export class Action {
   private readonly logger;
@@ -13,13 +16,43 @@ export class Action {
   }
 
   async run(inputs: Inputs) {
-    this.logger.info("Running github-action-nodejs-template");
-    const name = inputs.name || "World";
-    const message = `Hello ${name}`;
-    this.logger.info(message);
+    this.logger.info(`Copying files matching the pattern "${inputs.fileFilter || '**/*'}" from "${inputs.sourceDirectory}" to "${inputs.destinationDirectory}"`);
+
+    const files = glob.sync(inputs.fileFilter || '**/*', { cwd: inputs.sourceDirectory });
+    const copiedFiles: { destinationPath: string, count: number }[] = [];
+
+    files.forEach(file => {
+      const relativePath = path.relative(inputs.sourceDirectory, file);
+      const destinationPath = inputs.flattenDirectories
+        ? path.join(inputs.destinationDirectory, path.basename(file))
+        : path.join(inputs.destinationDirectory, relativePath);
+
+      const destinationDir = path.dirname(destinationPath);
+      if (!fs.existsSync(destinationDir)) {
+        fs.mkdirSync(destinationDir, { recursive: true });
+      }
+
+      fs.copyFileSync(file, destinationPath);
+      this.logger.info(`Copied '${file}' to '${destinationPath}'`);
+
+      const existingEntry = copiedFiles.find(entry => entry.destinationPath === destinationPath);
+      if (existingEntry) {
+        existingEntry.count += 1;
+      } else {
+        copiedFiles.push({ destinationPath: destinationPath, count: 1 });
+      }
+    });
+
+    // Generate markdown table
+    let markdownTable = "| Destination Path | Files Copied |\n| --- | --- |\n";
+    copiedFiles.forEach(entry => {
+      markdownTable += `| ${entry.destinationPath} | ${entry.count} |\n`;
+    });
+
+    this.outputs.save("copiedFiles", markdownTable);
+    this.logger.info("Files copied:\n" + markdownTable);
+
     await sleep(3000);
-    this.logger.info("Change: 10");
-    this.outputs.save("message", message);
     this.logger.info("Finished github-action-nodejs-template");
   }
 }
--- src/inputs/core-inputs.ts
+++ src/inputs/core-inputs.ts
@@ -1,9 +1,21 @@
 import { getInput } from "@actions/core";
-
 import { Inputs } from "./inputs";
 
 export class CoreInputs implements Inputs {
-  get name(): string | undefined {
-    return getInput("name") || undefined;
+  get sourceDirectory(): string {
+    return getInput("source-directory", { required: true });
+  }
+
+  get destinationDirectory(): string {
+    return getInput("destination-directory", { required: true });
+  }
+
+  get fileFilter(): string | undefined {
+    return getInput("file-filter") || undefined;
+  }
+
+  get flattenDirectories(): boolean {
+    const input = getInput("flatten-directories");
+    return input ? input.toLowerCase() === 'true' : false;
   }
 }
